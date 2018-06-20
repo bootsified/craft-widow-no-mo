96d799e264d06089527e2213271de4100caeaec3
Boots Highland <boots@simplygoodwork.com>
2018-06-20 16:20:22 -0500

    ALL DA FILEZZZZZ!!!!!!!!

---
+++ src/translations/en/craft-widow-no-mo.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * craft-widow-no-mo plugin for Craft CMS 3.x
+ *
+ * Preventz 'widows' in ur outputz.
+ *
+ * @link      http://boots.media
+ * @copyright Copyright (c) 2018 Boots
+ */
+
+/**
+ * craft-widow-no-mo en Translation
+ *
+ * Returns an array with the string to be translated (as passed to `Craft::t('craft-widow-no-mo', '...')`) as
+ * the key, and the translation as the value.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
+ *
+ * @author    Boots
+ * @package   Craftwidownomo
+ * @since     1.0.0
+ */
+return [
+    'craft-widow-no-mo plugin loaded' => 'craft-widow-no-mo plugin loaded',
+];
---
+++ src/twigextensions/CraftwidownomoTwigExtension.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * craft-widow-no-mo plugin for Craft CMS 3.x
+ *
+ * Preventz 'widows' in ur outputz.
+ *
+ * @link      http://boots.media
+ * @copyright Copyright (c) 2018 Boots
+ */
+
+namespace bootsified\craftwidownomo\twigextensions;
+
+use bootsified\craftwidownomo\Craftwidownomo;
+
+use Craft;
+
+/**
+ * Twig can be extended in many ways; you can add extra tags, filters, tests, operators,
+ * global variables, and functions. You can even extend the parser itself with
+ * node visitors.
+ *
+ * http://twig.sensiolabs.org/doc/advanced.html
+ *
+ * @author    Boots
+ * @package   Craftwidownomo
+ * @since     1.0.0
+ */
+class CraftwidownomoTwigExtension extends \Twig_Extension
+{
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName()
+    {
+        return 'Craftwidownomo';
+    }
+
+    /**
+     * Returns an array of Twig filters, used in Twig templates via:
+     *
+     *      {{ 'a string in your template' | nomowidow }}
+     *
+     * @return array
+     */
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('widownomo', [$this, 'widowNoMo']),
+        ];
+    }
+
+    /**
+     * Returns an array of Twig functions, used in Twig templates via:
+     *
+     *      {% set this = nomowidow('a string in your template') %}
+     *
+    * @return array
+     */
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('widownomo', [$this, 'widowNoMo']),
+        ];
+    }
+
+    /**
+     * Replaces the space between the nth-last words in a string with ``&nbsp;``
+     * Works in these block tags ``(h1-h6, p, li)`` and also accounts for
+     * potential closing inline elements ``a, em, strong, span, b, i``
+     *
+     * @param   $text Text to transform
+     * @param   $numberOfWords Number of words to force break
+     * @param   $outputRaw If we should output raw or not
+     *
+     * @return  The string with widows (hopefully) eliminated
+     */
+    public function widowNoMo($text = "", $numberOfWords = 1, $outputRaw = true)
+    {
+        $tags = "a|span|i|b|em|strong|acronym|caps|sub|sup|abbr|big|small|code|cite|tt";
+
+		// Taken from https://github.com/davethegr8/cakephp-typogrify-helper/blob/master/views/helpers/typogrify.php
+		// This regex is a beast, tread lightly
+		$regex = "/([^\s])\s+(((<($tags)[^>]*>)*\s*[^\s<>]+)(<\/($tags)>)*[^\s<>]*\s*(<\/(p|h[1-6]|li)>|$))/i";
+		$string = $text;
+
+		for ($i = 0; $i < $numberOfWords; $i++) {
+			$string = preg_replace($regex, '$1&nbsp;$2', $string);
+		}
+
+		return $outputRaw ? TemplateHelper::getRaw($string) : $string;
+    }
+}
